f95b50c7a0ab3e5efe53114bc48f9fe25773b9b2
木婉清 <1154938019@qq.com>
2025-02-10 16:54:46 +0800

    add

--- fitness.py
+++ fitness.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from 03-routine!")
+import streamlit as st
 
+def get_user_name():
+    return 'John'
 
-if __name__ == "__main__":
-    main()
+with st.echo():
+    # Everything inside this block will be both printed to the screen
+    # and executed.
+
+    def get_punctuation():
+        return '!!!'
+
+    greeting = "Hi there, "
+    value = get_user_name()
+    punctuation = get_punctuation()
+
+    st.write(greeting, value, punctuation)
+
+# And now we're back to _not_ printing to the screen
+foo = 'bar'
+st.write('Done!')
